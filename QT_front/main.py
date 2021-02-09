6aae958da6f060c07b18f784c3877345b0840289
amirhoseinaghaei <amirhoseinaghaeii@yahoo.com>
2021-02-10 02:21:17 +0330

    front of desktop app finished and now we have a program without any bug also l have created exe file and setup file of desktop app

--- QT_front/main.py
+++ QT_front/main.py
@@ -9,13 +9,13 @@ from PyQt5.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime
 from PyQt5.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
 import f1_rc
 from fpdf import FPDF
-from form1 import create_form1
-from form2 import create_form2
-from form3 import create_form3
+from Backend.form1 import create_form1
+from Backend.form2 import create_form2
+from Backend.form3 import create_form3
 import numpy as np 
-from animation import toggleMenu
+from Animation.animation import toggleMenu
 
-Form  = uic.loadUiType(os.path.join(os.getcwd() , "Desktop_1.ui"))[0]
+Form  = uic.loadUiType(os.path.join(os.getcwd() , "main.ui"))[0]
 
 
 
@@ -31,16 +31,18 @@ class Practice(QMainWindow , Form):
         self.Second_form_b.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_2))
         self.Third_form_b.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_3))
         self.Btn_Toggle.clicked.connect(lambda: toggleMenu(self, 250, True))
-        self.btn_page_1.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_1))
-        self.btn_page_2.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_2))
-        self.btn_page_3.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_3))
+        self.btn_page_1.clicked.connect(self.change_btn1)
+        self.btn_page_2.clicked.connect(self.change_btn2)
+        self.btn_page_3.clicked.connect(self.change_btn3)
+        self.btn_page_4.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_11))
+        self.btn_page_5.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_5))
         self.F2_NEXT_2.clicked.connect(self.f1_p_next)
-        self.F3_NEXT_2.clicked.connect(lambda: self.stackedWidget_f1.setCurrentWidget(self.F1_p4))
-        self.F3_PREVIOUS_2.clicked.connect(lambda: self.stackedWidget_f1.setCurrentWidget(self.F1_p2))
-        self.F4_NEXT_2.clicked.connect(lambda: self.stackedWidget_f1.setCurrentWidget(self.F1_p5))
-        self.F4_PREVIOUS_2.clicked.connect(lambda: self.stackedWidget_f1.setCurrentWidget(self.F1_p3))
+        self.F3_NEXT_2.clicked.connect(lambda: self.stackedWidget_f1.setCurrentWidget(self.F1_p5))
+        self.F3_PREVIOUS_2.clicked.connect(lambda: self.stackedWidget_f1.setCurrentWidget(self.F1_p4))
+        self.F4_NEXT_2.clicked.connect(lambda: self.stackedWidget_f1.setCurrentWidget(self.F1_p3))
+        self.F4_PREVIOUS_2.clicked.connect(lambda: self.stackedWidget_f1.setCurrentWidget(self.F1_p2))
         self.F5_NEXT_2.clicked.connect(lambda: self.stackedWidget_f1.setCurrentWidget(self.F1_p6))
-        self.F5_PREVIOUS_2.clicked.connect(lambda: self.stackedWidget_f1.setCurrentWidget(self.F1_p4))
+        self.F5_PREVIOUS_2.clicked.connect(lambda: self.stackedWidget_f1.setCurrentWidget(self.F1_p3))
         self.F6_NEXT_2.clicked.connect(lambda: self.stackedWidget_f1.setCurrentWidget(self.F1_p7))
         self.F6_PREVIOUS_2.clicked.connect(lambda: self.stackedWidget_f1.setCurrentWidget(self.F1_p5))
         self.F7_NEXT_2.clicked.connect(lambda: self.stackedWidget_f1.setCurrentWidget(self.F1_p8))
@@ -128,7 +130,13 @@ class Practice(QMainWindow , Form):
         self.title_list_w_f3 = []
         self.place_list_w_f3 = []
         self.summary_list_w_f3 = []
-
+        self.education_list_f2 = []
+        self.education_list_f3 = []
+        self.work_list = []
+        self.work_list_f2 = []
+        self.work_list_f3 = []
+        self.award_list = []
+        self.education_list = []
         self.skill_slider_f3.setMinimum(1)
         self.skill_slider_f3.setMaximum(10)
         self.skill_slider_f3.setTickInterval(1)
@@ -152,25 +160,49 @@ class Practice(QMainWindow , Form):
         self.F2_NEXT_2.clicked.connect(self.F_1_personal_values)
         self.F1_NEXT_2.clicked.connect(self.F_2_personal_values)
         self.F3_NEXT_4.clicked.connect(self.F_3_personal_values)
-        self.F4_NEXT_2.clicked.connect(self.get_value)
-        self.F3_NEXT_2.clicked.connect(self.get_value)
-        self.F5_NEXT_2.clicked.connect(self.get_value)
-        self.F6_NEXT_2.clicked.connect(self.get_value)
-        self.F7_NEXT_2.clicked.connect(self.get_value)
-        self.F1_NEXT_2.clicked.connect(self.get_value)
-        self.F6_NEXT_6.clicked.connect(self.get_value)
-        self.F3_NEXT_4.clicked.connect(self.get_value)
-        self.F1_NEXT_10.clicked.connect(self.get_value)
-        self.F1_NEXT_9.clicked.connect(self.get_value)
-        self.F5_NEXT_8.clicked.connect(self.get_value)
-        self.F6_NEXT_8.clicked.connect(self.get_value)
         self.f1_create_button.clicked.connect(self.create_f1)
         self.f1_create_button_5.clicked.connect(self.create_f2)
         self.f1_create_button_7.clicked.connect(self.create_f3)
         self.education_date_list = []
         self.Summary_f1 = self.Summary_F1.toPlainText().split(" ")
-
+        self.rl1.clicked.connect(self.remove_l1)
+        self.rs1.clicked.connect(self.remove_s1)
+        self.ra1.clicked.connect(self.remove_a1)
+        self.rw1.clicked.connect(self.remove_w1)
+        self.re1.clicked.connect(self.remove_e1)
+        self.rl2.clicked.connect(self.remove_l2)
+        self.rs2.clicked.connect(self.remove_s2)
+        self.re2.clicked.connect(self.remove_e2)
+        self.rw2.clicked.connect(self.remove_w2)
+        self.rl3.clicked.connect(self.remove_l3)
+        self.rs3.clicked.connect(self.remove_s3)
+        self.re3.clicked.connect(self.remove_e3)
+        self.rw3.clicked.connect(self.remove_w3)
     
+
+    def change_btn1(self) : 
+        self.stackedWidget.setCurrentWidget(self.page_1)
+        self.stackedWidget_f1.setCurrentWidget(self.F1_p2)
+    def change_btn2(self) : 
+        self.stackedWidget.setCurrentWidget(self.page_2)
+        self.stackedWidget_2.setCurrentWidget(self.page)
+
+    def change_btn3(self) : 
+        self.stackedWidget.setCurrentWidget(self.page_3)
+        self.stackedWidget_3.setCurrentWidget(self.page_10)
+        self.First_name_f3.setStyleSheet("background-color : #FFFFFF;")
+        self.Last_name_F3.setStyleSheet("background-color : #FFFFFF;")
+        self.Feild_name_f3.setStyleSheet("background-color : #FFFFFF;")
+        self.Phone_F3.setStyleSheet("background-color : #FFFFFF;")
+        self.Email_F3.setStyleSheet("background-color : #FFFFFF;")
+        self.Website_F3.setStyleSheet("background-color : #FFFFFF;")
+        self.pic_lineEdit_1.setStyleSheet("background-color : #FFFFFF;")
+
+
+
+
+
+
 
     def f1_p_next(self):
         if self.First_name_f1.text() == "" :
@@ -579,6 +611,8 @@ class Practice(QMainWindow , Form):
                 self.Website_F3.setStyleSheet("color : #bfbfbf;")
                 self.pic_lineEdit_1.setStyleSheet("color : #bfbfbf;")
                 self.stackedWidget_3.setCurrentWidget(self.page_13) 
+
+
     def add_repo(self):
         
         if self.Repo_line_edit_F1.text() not in self.repo_list:
@@ -598,7 +632,6 @@ class Practice(QMainWindow , Form):
     def F_1_personal_values(self):
             address = [self.country_str.text() , self.City_str.text() , self.Street_str.text() , self.Num_str.text()]   
             github = []
-            
             github.append(self.Github_F1.text())
             github.extend(self.repo_list)
             self.personal_list = [
@@ -609,7 +642,6 @@ class Practice(QMainWindow , Form):
             self.Email_F1.text() , 
             github ,  
             ]
-            print(self.personal_list)
             github = []
             return self.personal_list
 
@@ -693,9 +725,7 @@ class Practice(QMainWindow , Form):
 
     def add_F_1_education_values(self):
         
-
             if self.un_line_edit_f1.text() not in self.grade_list :
-                self.education_list = []
                 self.grade_list.append(self.un_line_edit_f1.text())
                 education_str = self.from_date_edit_f1.text() +  " - "  + self.to_date_edit_f1.text()   
                 self.education_date_list.append(education_str)  
@@ -712,13 +742,9 @@ class Practice(QMainWindow , Form):
                 self.from_date_edit_f1.setText("")
                 self.to_date_edit_f1.setText("")
 
-                
-            
     def add_F_2_education_values(self):
         
-
             if self.title_edit_f1.text() not in self.title_list_f2 :
-                self.education_list_f2 = []
                 self.title_list_f2.append(self.title_edit_f1.text())
                 education_str = self.from_date_edit_f1_5.text() +  " - "  + self.to_date_edit_f1_5.text()   
                 self.education_date_list_f2.append(education_str)  
@@ -735,9 +761,7 @@ class Practice(QMainWindow , Form):
                 self.to_date_edit_f1_5.setText("")
     def add_F_3_education_values(self):
         
-
             if self.title_edit_f1_3.text() not in self.title_list_f3 :
-                self.education_list_f3 = []
                 self.title_list_f3.append(self.title_edit_f1_3.text())
                 education_str = self.from_date_edit_f1_7.text() +  " - "  + self.to_date_edit_f1_7.text()   
                 self.education_date_list_f3.append(education_str)  
@@ -758,9 +782,7 @@ class Practice(QMainWindow , Form):
 
     def add_F_1_work_values(self):
 
-
            if self.title_line_edit_f1_2.text() not in self.title_list :
-                self.work_list = []
                 work_str = self.from_w_date_edit_f1.text() +  " - "  + self.to_w_date_edit_f1.text()   
                 self.title_list.append(self.title_line_edit_f1_2.text()) 
                 self.work_date_list.append(work_str)  
@@ -777,8 +799,6 @@ class Practice(QMainWindow , Form):
 
     def add_F_2_work_values(self):
         
-
-            self.work_list_f2 = []
             self.title_list_w_f2.append(self.title_line_edit_f1_6.text())
             education_str = self.from_w_date_edit_f1_5.text() +  " - "  + self.to_w_date_edit_f1_5.text()   
             self.education_date_list_w_f2.append(education_str)  
@@ -795,8 +815,6 @@ class Practice(QMainWindow , Form):
             self.from_w_date_edit_f1.setText("")
     def add_F_3_work_values(self):
         
-
-            self.work_list_f3 = []
             self.title_list_w_f3.append(self.title_line_edit_f1_8.text())
             education_str = self.from_w_date_edit_f1_7.text() +  " - "  + self.to_w_date_edit_f1_7.text()   
             self.education_date_list_w_f3.append(education_str)  
@@ -816,8 +834,6 @@ class Practice(QMainWindow , Form):
     def add_F_1_award_values(self):
 
             if self.award_summary_text_edit_f1.toPlainText() not in self.title_award_list :
-                    
-                self.award_list = []
                 award_str = self.award_date_edit_f1.text()
                 self.title_award_list.append(self.award_summary_text_edit_f1.toPlainText()) 
                 self.award_date_list.append(award_str)    
@@ -825,12 +841,70 @@ class Practice(QMainWindow , Form):
                 self.award_list.append(self.title_award_list)
                 self.award_summary_text_edit_f1.setText("")
                 self.award_date_edit_f1.setText("")
+    
 
+    
+    def remove_l1(self):
+        self.lang_list = []
+        self.lang_level_list = []
 
+    def remove_s1(self):
+        self.skill_list = []
+        self.skill_level_list = []
+    def remove_e1(self):
+        self.education_list = []
+        self.grade_list = []
+        self.education_date_list = [] 
+        self.place_list =  []
+        self.gpa_list = []
+        self.summary_list =[]
 
-    def get_value(self) : 
-        pass
+    def remove_w1(self):
+        self.title_list = []
+        self.work_date_list = []
+        self.summary_list_1 = []
+        self.work_list = []
+    def remove_a1(self):
+        self.award_list = []
+        self.title_award_list = []
+        self.award_date_list = []
+    def remove_l2(self):
+        self.lang_list_f2 = []
+        self.lang_level_list_f2 = []
+    def remove_s2(self):
+        self.skill_list_f2 = []
+        self.skill_level_list_f2 = []
+    def remove_e2(self):
+        self.education_list_f3 = []
+        self.title_list_f3 = []
+        self.education_date_list_f3 = []
+        self.place_list_f3 = []
+        self.summary_list_f3 = []
 
+    def remove_w2(self):
+        self.work_list_f2 = []
+        self.title_list_w_f2 = []
+        self.education_date_list_w_f2 = []
+        self.place_list_w_f2 = []
+        self.summary_list_w_f2 = []
+    def remove_l3(self):
+        self.lang_list_f3 = []
+        self.lang_level_list_f3 = []
+    def remove_s3(self):
+        self.skill_list_f3 = []
+        self.skill_level_list_f3 = []
+    def remove_e3(self):
+        self.education_list_f3 = []
+        self.title_list_f3 = []
+        self.education_date_list_f3 = [] 
+        self.place_list_f3 = []
+        self.summary_list_f3 = []
+    def remove_w3(self):
+        self.work_list_f3 = []
+        self.title_list_w_f3 = []
+        self.education_date_list_w_f3 = []
+        self.place_list_w_f3 = []
+        self.summary_list_w_f3 = []
 
 
     def create_f1(self):
@@ -838,16 +912,22 @@ class Practice(QMainWindow , Form):
         skills = [self.skill_list , self.skill_level_list ]
         language =  [self.lang_list  , self.lang_level_list ]
         create_form1(self.personal_list,skills,language,self.Summary_f1,self.work_list,self.education_list,self.award_list,font)
+        Str = "your resume created at main directory"
+        self.label_73.setText(Str)
     def create_f2(self):
         font = [str(self.font_f2_combo.currentText()) , self.fontsize_f2_spinBox.value()]
         skills = [self.skill_list_f2 , self.skill_level_list_f2 ]
         language =  [self.lang_list_f2 , self.lang_level_list_f2 ]
         create_form2(self.personal_list_f2,self.education_list_f2,self.work_list_f2,language,skills,font)
+        Str = "your resume created at {x}".format(x = self.pic_lineEdit.text() )
+        self.label_74.setText(Str)
     def create_f3(self):
         font = [str(self.font_f2_combo_2.currentText()) , self.fontsize_f2_spinBox_3.value()]
         skills = [self.skill_list_f3 , self.skill_level_list_f3 ]
         language =  [self.lang_list_f3 , self.lang_level_list_f3 ]
         create_form3(self.personal_list_f3 , skills , language , self.work_list_f3 , self.education_list_f3 , font)
+        Str = "your resume created at {x}".format(x = self.pic_lineEdit_1.text())
+        self.label_76.setText(Str)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv) 
